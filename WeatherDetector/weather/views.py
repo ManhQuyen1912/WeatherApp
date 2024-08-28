b5551a337d1293cb2b241cfd0d03cd3302af3ceb
ManhQuyen1912 <nmquyen1912@gmail.com>
2024-08-28 08:43:32 +0700

    first commit

---
+++ WeatherDetector/weather/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+import json
+import urllib.request
+
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=cb771e45ac79a4e8e2205c0ce66ff633').read()
+    else:
+        city = ''
+    return render(request,'index.html',{'city':city})
